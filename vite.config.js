86f5abcac355e83af9109db504c1225bec79f9c1
Jim Hu <wowdacom@gmail.com>
2023-08-17 11:46:27 +0100

    1. 新增 eslint + prettier 2. 新增 tailwind.css 3. 移除不必要的 style 4. 新增 iconify

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import Icons from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+    plugins: [
+        vue(),
+        Components({
+            dirs: ['src/components'],
+            resolvers: [IconsResolver()],
+        }),
+        Icons({
+            autoInstall: true,
+        }),
+    ],
+});
