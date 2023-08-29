73da2afdcdb01b382403c29dfb8229c894490661
Jim Hu <wowdacom@gmail.com>
2023-08-29 10:14:06 +0100

    遊戲完整流程完成

--- src/components/Playboard.vue
+++ src/components/Playboard.vue
@@ -2,8 +2,10 @@
 import { ref, reactive, watch } from 'vue';
 import { isMobile } from '../utils/device-detect';
 import cluesData from '../assets/clues.json';
+import clueDefaultPosition from '../assets/clues_position.json';
 
-const isGameStart = ref(true);
+const isGameStart = ref(false);
+const isGameEnd = ref(false);
 const isShowTip = ref(false);
 const isShowInstructions = ref(false);
 const isShowHint = ref(false);
@@ -64,6 +66,7 @@ const currentTimelinePosition = ref([
         y: 520,
     },
 ]);
+
 const answeringStyleRaw = reactive({
     timelineContainer: {
         paddingTop: '180px',
@@ -102,6 +105,8 @@ const answeringStyleRaw = reactive({
     },
 });
 
+const overOutlineCount = ref(0);
+
 const answeringStyle = reactive(JSON.parse(JSON.stringify(answeringStyleRaw)));
 
 const handleClueCardClick = (cardIndex) => {
@@ -115,6 +120,7 @@ const handleClueCardClick = (cardIndex) => {
 
 const handleClueCardTouch = (cardIndex, ev) => {
     console.log('touch');
+    isShowTip.value = false;
     currentClueCardEl.value = clueCardEl.value[cardIndex];
     document.body.append(currentClueCardEl.value);
     setCurrentClueCardMove(ev.touches[0].pageX, ev.touches[0].pageY - currentClueCardEl.value.offsetHeight / 2);
@@ -133,16 +139,46 @@ const handleClueCardClickOff = (cardIndex) => {
 const handleClueCardTouchOff = (cardIndex) => {
     clueCardContainerEl.value.append(currentClueCardEl.value);
     setCurrentClueCardMove(0, 0);
-    isShowHint.value = false;
+    if (isShowHint.value) {
+        isShowHint.value = false;
+
+        timelineEvents.value.splice(overOutlineCount.value, 0, clues.value[cardIndex]);
+
+        clues.value.splice(cardIndex, 1);
+        handleScore();
+        timelineEvents.value.sort((a, b) => a.year - b.year);
+        if (gameStatus.currentStep < gameStatus.totalStep) {
+            gameStatus.currentStep += 1;
+        } else {
+            isGameEnd.value = true;
+        }
+    }
+
     answeringStyleRaw.timelineContainer.paddingTop = '180px';
     answeringStyleRaw.timelineEvents.forEach((element, index) => {
         answeringStyleRaw.timelineEvents[index].transform = `translate(-50%, ${currentTimelinePosition.value[index].y}px)`;
     });
 };
+const handleScore = () => {
+    let isCorrect = false;
+    let insertPostion = overOutlineCount.value;
+    if (insertPostion === 0) {
+        isCorrect = timelineEvents.value[insertPostion].year < timelineEvents.value[insertPostion + 1].year;
+    } else if (insertPostion === timelineEvents.value.length - 1) {
+        isCorrect = timelineEvents.value[insertPostion].year > timelineEvents.value[insertPostion - 1].year;
+    } else {
+        isCorrect = timelineEvents.value[insertPostion].year > timelineEvents.value[insertPostion - 1].year && timelineEvents.value[insertPostion].year < timelineEvents.value[insertPostion + 1].year;
+    }
+
+    gameStatus.stepCorrect[gameStatus.currentStep - 1] = isCorrect;
+    if (isCorrect) {
+        gameStatus.score += timelineEvents.value[insertPostion].point;
+    }
+};
 
 const handleClueCardMove = (ev) => {
     setCurrentClueCardMove(ev.touches[0].pageX, ev.touches[0].pageY - currentClueCardEl.value.offsetHeight / 2);
-    handleAnswering();
+    handleAnswerProcess();
 };
 
 const setCurrentClueCardMove = (left, top) => {
@@ -150,7 +186,7 @@ const setCurrentClueCardMove = (left, top) => {
     currentClueCardEl.value.style.top = top + 'px';
 };
 
-const handleAnswering = () => {
+const handleAnswerProcess = () => {
     const currentCardState = currentClueCardEl.value.getBoundingClientRect();
     const timelineElState = timelineEl.value.getBoundingClientRect();
     const timelineEventsElState = timelineEventsEl.value.map((el) => el.getBoundingClientRect());
@@ -169,35 +205,37 @@ const handleAnswering = () => {
     if (gameStatus.currentStep === 1) {
         if (currentCardState.bottom > timelineEventsElState[0].bottom) {
             answeringStyle.hint.top = `${85 + timelineEventHeight + hintHeight + 28}px`;
+            overOutlineCount.value = 1;
         } else {
             answeringStyle.hint.top = '85px';
+            overOutlineCount.value = 0;
         }
     }
 
     if (gameStatus.currentStep === 2) {
         //計算每張 timelineEvent 的中心線，如果超過就移動 hint
         let timelineEventCenterLines = timelineEventsElState.map((el) => window.scrollY + el.top + el.height / 2);
-        let overOutlineCount = timelineEventCenterLines.reduce((acc, cur) => {
+        overOutlineCount.value = timelineEventCenterLines.reduce((acc, cur) => {
             if (currentCardState.bottom > cur) {
                 return acc + 1;
             }
             return acc;
         }, 0);
-
         answeringStyleRaw.timelineEvents.forEach((element, index) => {
-            if (index < overOutlineCount) {
-                answeringStyleRaw.timelineEvents[index].transform = `translate(-50%, ${currentTimelinePosition.value[index].y - hintHeight}px)`;
+            if (index < overOutlineCount.value) {
+                answeringStyleRaw.timelineEvents[index].transform = `translate(-50%, ${currentTimelinePosition.value[index]?.y - hintHeight}px)`;
             } else {
-                answeringStyleRaw.timelineEvents[index].transform = `translate(-50%, ${currentTimelinePosition.value[index].y}px)`;
+                answeringStyleRaw.timelineEvents[index].transform = `translate(-50%, ${currentTimelinePosition.value[index]?.y}px)`;
             }
         });
-        answeringStyleRaw.hint.top = `${0 + overOutlineCount * (timelineEventHeight + 19)}px`;
+
+        answeringStyleRaw.hint.top = `${80 + overOutlineCount.value * (timelineEventHeight + 17)}px`;
     }
 
     if (gameStatus.currentStep === 3) {
         //計算每張 timelineEvent 的中心線，如果超過就移動 hint
         let timelineEventCenterLines = timelineEventsElState.map((el) => window.scrollY + el.top + el.height / 2);
-        let overOutlineCount = timelineEventCenterLines.reduce((acc, cur) => {
+        overOutlineCount.value = timelineEventCenterLines.reduce((acc, cur) => {
             if (currentCardState.bottom > cur) {
                 return acc + 1;
             }
@@ -205,19 +243,23 @@ const handleAnswering = () => {
         }, 0);
 
         answeringStyleRaw.timelineEvents.forEach((element, index) => {
-            if (index < overOutlineCount) {
-                answeringStyleRaw.timelineEvents[index].transform = `translate(-50%, ${currentTimelinePosition.value[index].y - hintHeight}px)`;
+            if (index < overOutlineCount.value) {
+                answeringStyleRaw.timelineEvents[index].transform = `translate(-50%, ${currentTimelinePosition.value[index]?.y - hintHeight}px)`;
             } else {
-                answeringStyleRaw.timelineEvents[index].transform = `translate(-50%, ${currentTimelinePosition.value[index].y}px)`;
+                answeringStyleRaw.timelineEvents[index].transform = `translate(-50%, ${currentTimelinePosition.value[index]?.y}px)`;
             }
         });
-        answeringStyleRaw.hint.top = `${-40 + overOutlineCount * (timelineEventHeight + 27)}px`;
+        if (overOutlineCount.value === 0) {
+            answeringStyleRaw.hint.top = '-40px';
+        } else {
+            answeringStyleRaw.hint.top = `${-40 + overOutlineCount.value * (timelineEventHeight + 27)}px`;
+        }
     }
 
     if (gameStatus.currentStep > 3) {
         //計算每張 timelineEvent 的中心線，如果超過就移動 hint
         let timelineEventCenterLines = timelineEventsElState.map((el) => window.scrollY + el.top + el.height / 2);
-        let overOutlineCount = timelineEventCenterLines.reduce((acc, cur) => {
+        overOutlineCount.value = timelineEventCenterLines.reduce((acc, cur) => {
             if (currentCardState.bottom > cur) {
                 return acc + 1;
             }
@@ -225,17 +267,17 @@ const handleAnswering = () => {
         }, 0);
 
         answeringStyleRaw.timelineEvents.forEach((element, index) => {
-            if (index < overOutlineCount) {
+            if (index < overOutlineCount.value) {
                 answeringStyleRaw.timelineEvents[index].transform = `translate(-50%, ${currentTimelinePosition.value[index].y - hintHeight}px)`;
             } else {
                 answeringStyleRaw.timelineEvents[index].transform = `translate(-50%, ${currentTimelinePosition.value[index].y}px)`;
             }
         });
 
-        if (overOutlineCount === 0) {
+        if (overOutlineCount.value === 0) {
             answeringStyleRaw.hint.top = '-40px';
         } else {
-            answeringStyleRaw.hint.top = `${-40 + overOutlineCount * ((timelineEventHeight + 28) / 2) + hintHeight / 2}px`;
+            answeringStyleRaw.hint.top = `${-40 + overOutlineCount.value * ((timelineEventHeight + 28) / 2) + hintHeight / 2}px`;
         }
     }
 };
@@ -243,6 +285,7 @@ const handleAnswering = () => {
 watch(
     () => gameStatus.currentStep,
     (currentStep) => {
+        currentTimelinePosition.value = clueDefaultPosition[gameStatus.currentStep - 1];
         if (currentStep === 1) {
             answeringStyleRaw.timelineEvents[0].transform = 'translate(-50%, 160px)';
         }
@@ -307,12 +350,29 @@ watch(
     },
 );
 
+const handleGameStart = () => {
+    isGameStart.value = true;
+    isShowTip.value = true;
+};
+
+const handleShowInstructions = () => {
+    isShowInstructions.value = !isShowInstructions.value;
+};
+
 const gameInit = () => {
-    gameStatus.currentStep = 6;
-    timelineEvents.value = cluesData.slice(0, gameStatus.currentStep);
+    timelineEvents.value.push({
+        year: '2023',
+        event: '2023 iThome鐵人賽',
+        description: '在開賽期間，選擇一天開賽，並且達成連續 30 天發表技術文章不中斷，即煉成鐵人完賽。',
+        image: 'https://picsum.photos/100/100?random=17',
+        translateY: '300px',
+        point: 0,
+        step: 0,
+    });
+
     clues.value = [...cluesData];
-    console.log(gameStatus.currentStep);
-    console.log(clues.value);
+
+    currentTimelinePosition.value = clueDefaultPosition[gameStatus.currentStep - 1];
 };
 gameInit();
 </script>
@@ -322,7 +382,7 @@ gameInit();
         <div class="w-full h-full flex justify-center items-center">
             <div class="w-full h-full relative" v-if="isGameStart">
                 <div class="mx-1 h-8 flex justify-center items-center relative">
-                    <div class="mr-2 text-sm font-Libre">{{ gameStatus.currentStep }} of {{ gameStatus.totalStep }}</div>
+                    <div class="mr-2 text-sm font-Libre">{{ gameStatus.currentStep }} / {{ gameStatus.totalStep }} 題</div>
                     <ul class="flex items-center">
                         <li
                             class="w-6 h-2.5 mr-[2px] border-2 rounded-full"
@@ -333,12 +393,12 @@ gameInit();
                             v-for="(isCorrect, index) in gameStatus.stepCorrect"
                         ></li>
                     </ul>
-                    <div class="ml-2 text-sm font-Libre">{{ gameStatus.score }} Points</div>
-                    <div class="ml-2 w-5 h-5 mt-0.5 flex justify-center items-center border-2 border-[#b1aea4] text-[#b1aea4] rounded-full">
+                    <div class="ml-2 text-sm font-Libre">{{ gameStatus.score }} 分</div>
+                    <div @click="handleShowInstructions" class="z-50 ml-2 w-5 h-5 mt-0.5 flex justify-center items-center border-2 border-[#b1aea4] text-[#b1aea4] rounded-full">
                         <i-healthicons-question-mark class="text-sm" />
                     </div>
                 </div>
-                <div ref="clueCardContainerEl" class="absolute h-[145px] left-1/2 top-8 z-10">
+                <div v-if="!isGameEnd" ref="clueCardContainerEl" class="absolute h-[145px] left-1/2 top-8 z-10">
                     <div
                         ref="clueCardEl"
                         v-for="(clue, index) in clues"
@@ -353,16 +413,22 @@ gameInit();
                         @dragstart="() => false"
                     >
                         <img class="w-[100px] h-[100px] mr-2 shrink-0" :src="clue.image" alt="" />
-                        <p class="text-sm font-bold">{{ clue.description }}</p>
-                        <div class="absolute right-2 bottom-2 font-Libre text-[#b1aea4] text-sm">{{ clue.point }} Points</div>
+                        <p class="text-sm font-bold">{{ clue.year + ' ' + clue.description }}</p>
+                        <div class="absolute right-2 bottom-2 font-Libre text-[#b1aea4] text-sm">{{ clue.point }} 分</div>
                     </div>
                     <div class="absolute bottom-10 right-24 translate-x-1/2 translate-y-10" v-if="isShowTip">
                         <i-carbon-touch-1-filled class="animate-[wiggle_5s_infinite_forwards] text-4xl text-yellow-400" />
                         <div class="absolute w-60 -bottom-10 -left-20 -rotate-3 font-bold">將線索拖曳到時間軌跡上！</div>
                     </div>
                 </div>
+                <div v-else class="absolute h-[145px] w-full top-8 z-10 flex flex-col justify-center items-center text-center bg-gray-500 text-white rounded-lg">
+                    <div class="text-2xl font-bold">遊戲結束</div>
+                    很厲害喔! <br />你共答對 {{ gameStatus.stepCorrect.filter((item) => item).length }} 題 <br />
+                    得分 {{ gameStatus.score }} 分
+                    <i-majesticons-lightbulb-shine class="absolute top-4 right-10 rotate-180 text-yellow-300 text-4xl"></i-majesticons-lightbulb-shine>
+                </div>
                 <div ref="timelineContainerEl" class="absolute bottom-0 left-1/2 -translate-x-1/2 h-full w-full px-4">
-                    <div :style="answeringStyle.timelineContainer" class="transition-all duration-700 relative h-full flex flex-col items-center">
+                    <div :style="answeringStyle.timelineContainer" class="transition-all duration-700 relative h-full flex flex-col items-center z-0">
                         <div ref="anchorBeforeEl" class="text-[#b1aea4]">BEFORE</div>
                         <div class="w-0.5 h-full bg-white"></div>
                         <div class="text-[#b1aea4] mb-4">AFTER</div>
@@ -394,50 +460,52 @@ gameInit();
                 </div>
             </div>
             <div v-else class="w-[350px] h-[350px] rounded-2xl border-2 flex flex-col justify-center items-center relative bg-white shadow-md">
-                <div class="h-5 absolute top-2 right-2 border-2 border-[#b1aea4] text-[#b1aea4] rounded-full">
+                <div @click="handleShowInstructions" class="h-5 absolute top-2 right-2 border-2 border-[#b1aea4] text-[#b1aea4] rounded-full">
                     <i-healthicons-question-mark class="text-sm" />
                 </div>
                 <i-clarity-beta-solid class="text-5xl text-[#5D72C8] opacity-30" />
                 <h1 class="text-4xl font-extrabold mb-3">時間線任務</h1>
                 <h4 class="mb-5 text-[#b1aea4]">2023年9月1日</h4>
                 <h2 class="mb-2 text-center">你能把八個重要事件<br />按照時間順序排列嗎？</h2>
-                <button class="rounded-full border w-[150px] h-[40px] bg-[#5d72c9] text-white">開始遊戲 <i-maki-arrow class="inline-block" /></button>
+                <button @click="handleGameStart" class="rounded-full border w-[150px] h-[40px] bg-[#5d72c9] text-white">開始遊戲 <i-maki-arrow class="inline-block" /></button>
             </div>
         </div>
 
-        <div v-if="isShowInstructions" class="mx-auto w-11/12 px-4 py-12 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg">
-            <i-iconoir-cancel class="absolute right-2 top-2 text-xl" />
-            <h1 class="text-2xl font-bold mb-6 text-center">怎麼玩時間線任務</h1>
-            <div class="">
-                <p class="mb-2">將每張線索卡按照時間順序放置在時間軌跡上：</p>
-                <div class="flex mb-4">
-                    <ol class="list-decimal pl-4 mb-4">
-                        <li class="mb-2">將線索拖曳至其他事件之間、之前或之後。</li>
-                        <li class="mb-2">將卡片從最早（頂部）排列至最晚（底部）。</li>
-                        <li class="mb-2">按下「點擊以放置」以確認位置。</li>
-                    </ol>
-                    <video
-                        id="how-to-animation"
-                        width="118"
-                        height="159"
-                        src="https://static01.nytimes.com/newsgraphics/2023-01-05-headlines/dbee7ea268717ea6be65b1bd8bbbb1a46bdf16b8/_assets/images/HowToAnimation-03-low.mp4"
-                        autoplay
-                        loop
-                        playsinline
-                        preload
-                        class="svelte-1jnn636"
-                    >
-                        <img
-                            src="https://static01.nytimes.com/newsgraphics/2023-01-05-headlines/dbee7ea268717ea6be65b1bd8bbbb1a46bdf16b8/_assets/images/HowToAnimation-03.jpg"
-                            title="Your browser does not support the <video> tag"
-                            alt="A short animation showing a card being dragged onto a timeline."
-                        />
-                    </video>
+        <Teleport to="body">
+            <div v-if="isShowInstructions" class="mx-auto w-11/12 px-4 py-12 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg z-20">
+                <i-iconoir-cancel @click="handleShowInstructions" class="absolute right-2 top-2 text-xl" />
+                <h1 class="text-2xl font-bold mb-6 text-center">怎麼玩時間線任務</h1>
+                <div class="">
+                    <p class="mb-2">將每張線索卡按照時間順序放置在時間軌跡上：</p>
+                    <div class="flex mb-4">
+                        <ol class="list-decimal pl-4 mb-4">
+                            <li class="mb-2">將線索拖曳至其他事件之間、之前或之後。</li>
+                            <li class="mb-2">將卡片從最早（頂部）排列至最晚（底部）。</li>
+                            <li class="mb-2">按下「點擊以放置」以確認位置。</li>
+                        </ol>
+                        <video
+                            id="how-to-animation"
+                            width="118"
+                            height="159"
+                            src="https://static01.nytimes.com/newsgraphics/2023-01-05-headlines/dbee7ea268717ea6be65b1bd8bbbb1a46bdf16b8/_assets/images/HowToAnimation-03-low.mp4"
+                            autoplay
+                            loop
+                            playsinline
+                            preload
+                            class="svelte-1jnn636"
+                        >
+                            <img
+                                src="https://static01.nytimes.com/newsgraphics/2023-01-05-headlines/dbee7ea268717ea6be65b1bd8bbbb1a46bdf16b8/_assets/images/HowToAnimation-03.jpg"
+                                title="Your browser does not support the <video> tag"
+                                alt="A short animation showing a card being dragged onto a timeline."
+                            />
+                        </video>
+                    </div>
+                    <p class="mb-2">錯誤的線索將在每回合移至正確的位置。正確的線索可獲得2至5分。完美分數為28分。</p>
+                    <p>編輯根據時報的報導選擇線索。每一份被推動的社會進步將會有一個新的問答遊戲。</p>
                 </div>
-                <p class="mb-2">錯誤的線索將在每回合移至正確的位置。正確的線索可獲得2至5分。完美分數為28分。</p>
-                <p>編輯根據時報的報導選擇線索。每一份被推動的社會進步將會有一個新的問答遊戲。</p>
             </div>
-        </div>
+        </Teleport>
     </div>
 </template>
 <style scoped>
